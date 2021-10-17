7bd949685154769950cea0ef12cb990af391cae5
Oleg Smatkov <MoHcTpUk@users.noreply.github.com>
2021-10-18 00:16:37 +0300

    Refactoring

---
+++ DevNullCore.Ioc/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using DevNullCore.Ioc.Extensions;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevNullCore.Ioc
+{
+    public static class DependencyContainer
+    {
+        public static void ConfigureDepencies(IServiceCollection serviceCollection)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(_ => !_.IsDynamic).ToList();
+
+            serviceCollection
+                .AddServices(assemblies)
+                .AddAutoMapperConfigs(assemblies)
+                .AddMediatR(assemblies.ToArray())
+                ;
+        }
+    }
+}
